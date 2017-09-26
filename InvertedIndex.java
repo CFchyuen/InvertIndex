d6c47ec04de6645fd11c8a9b5e59ee77177b83bd
Rafael Vasquez <rafael.vasquez@ryerson.ca>
2017-09-26 18:56:18 -0400

    initial commit

---
+++ InvertedIndex.java
@@ -0,0 +1,27 @@
+import java.io.file
+
+public class InvertedIndex {
+  
+   public static void main(String [] args) 
+   {
+      if(args.length > 0) {
+         File documentCollection = new File(args[0])
+      }
+      
+      Map<String, String> dictionary = new HashMap<String, String>();
+      
+      
+   }
+
+   public void invert(){
+      
+   }
+   
+   public void sortAlphabetically(Map<String, String> dictionary)
+   {
+   
+   }
+   
+   
+   
+}
