877cfa11102dd7a69268d1e1e68d715da5cc9c62
Cliff Yuen <chyuen@scs.ryerson.ca>
2017-09-27 15:53:23 -0400

    first commit

--- InvertedIndex.java
+++ InvertedIndex.java
@@ -1,20 +1,32 @@
-import java.io.file
+
+import java.util.*;
+import java.io.*;
 
 public class InvertedIndex {
   
    public static void main(String [] args) 
    {
-      if(args.length > 0) {
-         File documentCollection = new File(args[0])
-      }
+      File documentCollection = new File("cacm.all");
       
+      /**
+      if(args.length > 0) {
+         documentCollection = new File(args[0]);
+      } else {
+         System.out.println("Invalid input");
+      } **/
+      
+      
+      Map<String, String> dictionary = makeDictionary(documentCollection);
+   }
+
+
+   public static Map<String,String> makeDictionary(File file) {
       Map<String, String> dictionary = new HashMap<String, String>();
       
       
-   }
-
-   public void invert(){
       
+      
+      return dictionary;
    }
    
    public void sortAlphabetically(Map<String, String> dictionary)
---
+++ Posting.java
@@ -0,0 +1,10 @@
+public class Posting {
+   
+   public Posting(){
+      private String term;
+      private int docID;
+      private int termFreq;
+      private String highlight; //first 10 words
+     
+   }
+}
